8133dcb8837809c793b75f63ead403f51bfbb238
daze <leon.baiocchi@studio.unibo.it>
2023-10-30 14:30:21 +0100

    wrap profile front-end inside client requests to api

---
+++ js/profile/profileRequest.js
@@ -0,0 +1,43 @@
+import { ProfileInfo } from './view-components/profile-info/profileInfo.js';
+import { ProfileFeed } from './view-components/profile-feed/profileFeed.js';
+import { ProfileViewLayout } from './view-layouts/profileViewLayout.js';
+
+class ProfileRequest {
+    #components = {
+        "profileInfo": new ProfileInfo(),
+        "profileFeed": new ProfileFeed()
+    };
+    #layout = undefined;
+
+    #profileFeedCallback(userData, userFeedData, firstLoad=true) {
+        this.#layout = new ProfileViewLayout(this.#components, userData, userFeedData);
+        this.#layout.render(document.querySelector("main"));
+        if (firstLoad) {/*
+            attachProfileInfoListeners(userData);
+            attachProfileFeedListeners(userData, userFeedData);
+        } else {
+            detachProfileInfoListeners(userData);
+            detachProfileFeedListeners(userData, userFeedData);
+            attachProfileInfoListeners(userData);
+            attachProfileFeedListeners(userData, userFeedData);*/
+        }
+    }
+    
+    #profileInfoCallback(userData, firstLoad=true) {
+        axios.get("api/api-profile-feed.php").then(response => {
+            if (firstLoad) {
+                this.#profileFeedCallback(userData, response.data);
+            } else {
+                this.#profileFeedCallback(userData, response.data, firstLoad);
+            }
+        });
+    }
+    
+    loadRequest(firstLoad=true) {
+        axios.get("api/api-profile.php").then(response => {
+            this.#profileInfoCallback(response.data, firstLoad);
+        });
+    }
+}
+
+export { ProfileRequest }
