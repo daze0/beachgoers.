a1cc5764494fafec702159db43b35c77dfe7c10d
daze <leon.baiocchi@studio.unibo.it>
2024-04-26 22:32:42 +0200

    add popperjs

--- template/base.php
+++ template/base.php
@@ -16,6 +16,7 @@
     <nav class="p-0 navbar navbar-light bg-light"></nav>
     <main class="pb-4"></main>
     <footer class="ms-0 mt-4 me-0 p-4 align-middle border-top border-light"></footer>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <?php foreach ($webpage->getJs() as $jsScript) : ?>
         <script src="<?php echo $jsScript; ?>"></script>
