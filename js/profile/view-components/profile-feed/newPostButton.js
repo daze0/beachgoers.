1cb64af2d02b73ad6dab7f587a122473e5375a8b
daze <leon.baiocchi@studio.unibo.it>
2024-05-03 19:15:07 +0200

    fix btn variable

--- js/profile/view-components/profile-feed/newPostButton.js
+++ js/profile/view-components/profile-feed/newPostButton.js
@@ -9,7 +9,7 @@ class NewPostButton extends PopupOpenElement {
     generateComponent(isPersonalProfile) {
         let btn = undefined;
         if (isPersonalProfile) {
-            const btn = this._generateReactiveButton("newPostButton", "w-100", "bi-plus-circle");
+            btn = this._generateReactiveButton("newPostButton", "w-100", "bi-plus-circle");
         }
         return (btn instanceof HTMLElement) ? btn.outerHTML : "";
         /*
