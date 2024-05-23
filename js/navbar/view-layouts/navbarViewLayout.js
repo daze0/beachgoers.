dc4b1641f31cc971436777f66b03598848153c34
daze <leon.baiocchi@studio.unibo.it>
2024-05-23 21:22:39 +0200

    implement navbar responsive layout

--- js/navbar/view-layouts/navbarViewLayout.js
+++ js/navbar/view-layouts/navbarViewLayout.js
@@ -23,22 +23,22 @@ class NavbarViewLayout {
 
     #generate() {
         return `
-        <div class="container-fluid">
+        <div class="container-fluid py-4">
             <div class="d-flex flex-column flex-md-row w-100 align-items-center justify-content-md-between">
-                <div class="d-flex flex-row flex-md-col w-100 justify-content-center justify-content-md-start">
+                <div class="d-flex flex-row flex-md-col mb-2 mb-md-0 w-100 justify-content-center justify-content-md-start">
                     <a href="#" class="navbar-brand navbar-color mb-0 ms-2 h1">
                         beachgoers.
                     </a>
                 </div>
-                <div class="d-flex flex-row flex-md-col w-100 justify-content-center justify-content-md-end">
-                    <a href="feed.php" class="nav-link mb-0">
+                <div class="d-flex flex-row flex-md-col mb-2 mb-md-0 ms-2 ms-md-0 me-3 w-100 justify-content-center justify-content-md-end">
+                    <a href="feed.php" class="nav-link mb-0 me-2">
                         <i class="bi bi-house-fill fs-3 navbar-color"></i>
                     </a>
-                    <a href="profile.php" class="nav-link mb-0">
+                    <a href="profile.php" class="nav-link mb-0 me-2">
                         <i class="bi bi-person-fill fs-3 navbar-color"></i>
                     </a>
                     ${this.#components["notificationButton"].generateComponent().outerHTML}
-                    <a href="#" class="nav-link mb-0">
+                    <a href="#" class="nav-link mb-0 ms-2">
                         ${this.#components["logoutIcon"].generateComponent().outerHTML}
                     </a>
                 </div>
